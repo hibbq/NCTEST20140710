cdadf0ee357eba927a562dfebdb84347863ff120
devdb2012 <trend.nancychou+devdb2012@gmail.com>
2014-07-11 14:43:40 +0800

    add comment

--- WebApplication6/Default.aspx.cs
+++ WebApplication6/Default.aspx.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// test20140711-1-just comment---1231
+
 namespace WebApplication6
 {
     public partial class _Default : System.Web.UI.Page
