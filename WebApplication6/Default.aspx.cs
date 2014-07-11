d21c8c7d91e7ce2614e04445e1b890d013ddf617
hibbq <yesgogogo+hibbq@gmail.com>
2014-07-11 14:46:07 +0800

    Update Default.aspx.cs
    add comment from github

--- WebApplication6/Default.aspx.cs
+++ WebApplication6/Default.aspx.cs
@@ -6,7 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 // test20140711-1-just comment---1231
-
+// test20140711 : from github website
 namespace WebApplication6
 {
     public partial class _Default : System.Web.UI.Page
